d9ee5e41436b4d10795f313daa6eff6be432022a
DangerousMilk <galikgrisha@gmail.com>
2026-03-17 20:53:23 +0100

    Updated log reader.

--- LogReader.m
+++ LogReader.m
@@ -1,21 +1,51 @@
+close all;
+clear all;
+clc;
+
 graphics_toolkit('gnuplot');
 
 data = csvread('LOG.CSV', 1, 0);
 
 time = data(:,1) / 1000
 
+smoothing = 200;
+
 figure;
-plot(time, data(:,2));
 title('Druck');
+plot(time, movmean(data(:,2), smoothing));
+xlabel('t in s');
+ylabel('Druck in hPa');
 
 figure;
-plot(time, data(:,3));
-title('Höhe');
+title('Hoehe');
+plot(time, movmean(data(:,3), smoothing));
+xlabel('t in s');
+ylabel('Hoehe in m');
 
 figure;
-plot(time, data(:,4));
 title('Temperatur');
+plot(time, movmean(data(:,4), smoothing));
+xlabel('t in s');
+ylabel('Temp. in C');
+
+#plot(time, data(:,5:end));
+#title('GX, GY, GZ');
 
 figure;
-plot(time, data(:,5:end));
-title('GX, GY, GZ');
+title('Beschleunigung X');
+plot(time, movmean(data(:,5), smoothing));
+xlabel('t in s');
+ylabel('Beschleunigung X in m/s^2');
+
+figure;
+title('Beschleunigung Y');
+plot(time, movmean(data(:,6), smoothing));
+xlabel('t in s');
+ylabel('Beschleunigung Y in m/s^2');
+
+figure;
+title('Beschleunigung Z');
+plot(time, movmean(data(:,7), smoothing));
+xlabel('t in s');
+ylabel('Beschleunigung Z in m/s^2');
+
